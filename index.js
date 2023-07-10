6830eeac8f7cfb68a85e80ad7830826f57fda34e
Philip Vella <4180985+philipvella@users.noreply.github.com>
2023-07-10 12:22:34 +0100

    adding jest tests

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
-const csv = require('csv-parser');
+const fs = require("fs");
+const csv = require("csv-parser");
 
-const csvFilePath = 'input.csv';
-const jsonFilePath = 'output.json';
+const csvFilePath = "input.csv";
+const jsonFilePath = "output.json";
 
 const results = [];
 
 fs.createReadStream(csvFilePath)
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-        const jsonData = JSON.stringify(results, null, 2);
-        fs.writeFile(jsonFilePath, jsonData, (error) => {
-            if (error) {
-                console.error('Error writing JSON file:', error);
-            } else {
-                console.log('JSON file written successfully!');
-            }
-        });
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    const jsonData = JSON.stringify(results, null, 2);
+    fs.writeFile(jsonFilePath, jsonData, (error) => {
+      if (error) {
+        console.error("Error writing JSON file:", error);
+      } else {
+        console.log("JSON file written successfully!");
+      }
     });
+  });
